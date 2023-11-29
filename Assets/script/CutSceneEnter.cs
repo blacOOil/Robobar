5f154ce34f66b6ffed78fe8c3c0ecc63b0363dd7
blacOOil <111561598+blacOOil@users.noreply.github.com>
2023-11-29 22:13:04 +0700

    CutsceneSuccesful

--- Assets/script/CutSceneEnter.cs
+++ Assets/script/CutSceneEnter.cs
@@ -1,50 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine;
-
 
 public class CutSceneEnter : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject Player;
-    public CinemachineVirtualCamera CutSceneCam,PlaerCam;
+    public GameObject Player, CutSceneCam, PlaerCam;
     public float cutscenduration = 0f;
     private Rigidbody playerRigidbody;
 
     void Start()
     {
         playerRigidbody = Player.GetComponent<Rigidbody>();
-        CutSceneCam.Priority = 0;
-       
+
     }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            
-           
-            CutSceneCam.Priority =3;
             playerRigidbody.constraints = RigidbodyConstraints.FreezePosition;
-            BackToGame();
-           
+            StartCoroutine(fincut());
+            
         }
 
     }
     IEnumerator fincut()
     {
-        yield return new WaitForSeconds(1);
-        Debug.Log("Wait Successful");
-    }
-    public void BackToGame()
-    {
-        StartCoroutine(fincut());
-       // CutSceneCam.SetActive(false);
-       // PlaerCam.SetActive(true);
+        Debug.Log("startScene");
+        PlaerCam.gameObject.SetActive(false);
+        CutSceneCam.gameObject.SetActive(true);
+        Debug.Log("changeCam");
+        yield return new WaitForSeconds(4);
+        Debug.Log("EndScene");
+        PlaerCam.gameObject.SetActive(true);
         playerRigidbody.constraints = RigidbodyConstraints.None;
-
+        Destroy(gameObject);
     }
-   
-
-   
 }
