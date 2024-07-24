c631b4902096d8d2ac48b379a730eb03f3c5ee2a
blacOOil <111561598+blacOOil@users.noreply.github.com>
2024-07-24 15:42:07 +0700

    finishCharacterSelection

--- Assets/script/CharacterSelection.cs
+++ Assets/script/CharacterSelection.cs
@@ -7,19 +7,22 @@ public class CharacterSelection : MonoBehaviour
 {
     public TimerCode timerCode;
     public MonneyLevelCode monneyLevelCode;
-    public bool isCharacterSelected;
+    public bool isCharacterSelected,IsReadyToPlay,IsplayerSpawned;
     public GameObject CharSelectedZone,MainGameUi,SelectingUi,CharacterSelector;
     public List<GameObject> CharacterList;
+    public Transform PlayerSpawnPoint;
 
     public List<Transform> CharacterTranformList;
     public int selectorNumber = 0;
     // Start is called before the first frame update
     void Start()
     {
+        IsplayerSpawned = false;
         isCharacterSelected = false;
         timerCode.enabled = false;
         monneyLevelCode.enabled = false;
         MainGameUi.SetActive(false);
+        IsReadyToPlay = false;
 
         foreach(GameObject character in CharacterList)
         {
@@ -36,32 +39,52 @@ public class CharacterSelection : MonoBehaviour
             monneyLevelCode.enabled = true;
             MainGameUi.SetActive(true);
             CharSelectedZone.SetActive(false);
+            SpawnPlayer();
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        else
         {
-            isCharacterSelected = true;
-        }
-        if (!isCharacterSelected)
-        {
-            if (Input.GetKeyDown(KeyCode.D))
+            if (Input.GetKeyDown(KeyCode.D) && (selectorNumber < CharacterTranformList.Count))
             {
                 selectorNumber++;
                 SelectorMove();
             }
-            if ( selectorNumber >= 0 &&(Input.GetKeyDown(KeyCode.A)))
+            if (selectorNumber > 0 && (Input.GetKeyDown(KeyCode.A)))
             {
                 selectorNumber--;
                 SelectorMove();
             }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                IsReadyToPlay = true;
+                isCharacterSelected = true;
+
+            }   
         }
     }
     public void SelectorMove()
     {
        
        
-        if(selectorNumber >= 0 && selectorNumber < CharacterTranformList.Count)
+        if(selectorNumber >= 0 && (selectorNumber < CharacterTranformList.Count))
         {
             CharacterSelector.transform.position = CharacterTranformList[selectorNumber].position;
+        }
+    }
+    public void SpawnPlayer() 
+    {
+        if (!IsplayerSpawned)
+        {
+            CharacterList[selectorNumber].transform.position = PlayerSpawnPoint.position;
+            IsplayerSpawned = true;
+        }
+       
+        foreach (GameObject character in CharacterList)
+        {
+            BotController botController = character.GetComponent<BotController>();
+            botController.enabled = true;
+            ServiceSystem service = character.GetComponent<ServiceSystem>();
+            service.enabled = true;
+
         }
     }
 }
