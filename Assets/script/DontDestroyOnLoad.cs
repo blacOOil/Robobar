75577142dd39b013b296a1eb836474fb66082dd7
blacOOil <111561598+blacOOil@users.noreply.github.com>
2024-12-12 12:24:17 +0700

    Update Dont dest

--- Assets/script/DontDestroyOnLoad.cs
+++ Assets/script/DontDestroyOnLoad.cs
@@ -2,32 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DontDestroyOnLoad : MonoBehaviour
+public class noDestroyOnLoad : MonoBehaviour
 {
-    public GameObject settingPanel;
-    public static DontDestroyOnLoad instance;
+    public GameObject settingPanel,SettingCanvas;
+    public static noDestroyOnLoad instance;
     
-    void Awake() {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+    void Start() 
+    {
+       
+        // Set this instance as the singleton
+        instance = this;
 
+        // Ensure DontDestroyOnLoad is called before any potential destruction
+        DontDestroyOnLoad(gameObject);
+        // Check if an instance already exists
+        Debug.Log("DontDestroyOnLoad: Object marked as persistent.");
+      
+
+     
+
+        
     }
 
+
+
     void Update() {
+        if (SettingCanvas == null)
+        {
+            SettingCanvas = GameObject.Find("MainCanvas");
+        }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (settingPanel != null)
             {
-                // Toggle the active state of the settings panel
-                bool isActive = settingPanel.activeSelf;
-                settingPanel.SetActive(!isActive);
+
+                    GameObject newObject = Instantiate(settingPanel, Vector3.zero, Quaternion.identity);
+                    newObject.transform.SetParent(SettingCanvas.transform);
+                newObject.transform.localPosition = new Vector3(100, 50, 0); // Example position
+                newObject.transform.localScale = new Vector3(0.5f, 0.5f, 1f); // Example scale
+
             }
             else
             {
